fdfb5a9e56e506c96ec31c9a592da989b748d27a
Helias <stefanoborzi32@gmail.com>
2021-05-31 00:46:01 +0200

    fix: ci, build

--- src/AntiFarming.cpp
+++ src/AntiFarming.cpp
@@ -18,12 +18,12 @@ public: AntiFarmingPlayerScript() : PlayerScript("AntiFarmingPlayerScript") {}
 
         void OnPVPKill(Player* killer, Player* killed) override {
             if (sConfigMgr->GetBoolDefault("AntiFarming.Enable", true)) {
-                uint32 KillerGUID = killer->GetGUID();
-                uint32 VictimGUID = killed->GetGUID();
+                uint32 KillerGUID = killer->GetGUID().GetCounter();
+                uint32 VictimGUID = killed->GetGUID().GetCounter();
 
                 if (KillerGUID == VictimGUID) // Suicide
                     return;
-				
+
                 if (killer->GetSession()->GetRemoteAddress() == killed->GetSession()->GetRemoteAddress() || killed->GetMaxHealth() < (uint32)sConfigMgr->GetIntDefault("AntiFarming.MinHealth", 10000)) { // about 1/4 of the maximum hp a character can have on your server to make this fully functional.
                     sAntiFarming->dataMap[KillerGUID]++;
                     std::string str = "|cFFFFFC00[Anti-Farm System]|cFF00FFFF[|cFF60FF00" + killer->GetName() + "|cFF00FFFF] Possible Farmer!";
@@ -50,30 +50,7 @@ public: AntiFarmingPlayerScript() : PlayerScript("AntiFarmingPlayerScript") {}
         }
 };
 
-class AntiFarmingWorldScript : public WorldScript {
-public:
-    AntiFarmingWorldScript() : WorldScript("AntiFarmingWorldScript") { }
-
-    void OnBeforeConfigLoad(bool reload) override {
-        if (!reload) {
-            std::string conf_path = _CONF_DIR;
-            std::string cfg_file = conf_path + "/AntiFarming.conf";
-            #ifdef WIN32
-                cfg_file = "AntiFarming.conf";
-            #endif // WIN32
-            std::string cfg_def_file = cfg_file + ".dist";
-            sConfigMgr->LoadMore(cfg_def_file.c_str());
-
-            sConfigMgr->LoadMore(cfg_file.c_str());
-        }
-    }
-    void OnAfterConfigLoad(bool /*reload*/) override {
-        sLog->outString("Antifarming Module Loaded.");
-    }
-};
-
 void AddAntiFarmingScripts() {
     new AntiFarmingPlayerScript();
-    new AntiFarmingWorldScript();
     AddSC_antifarming_commandscript();
 }
